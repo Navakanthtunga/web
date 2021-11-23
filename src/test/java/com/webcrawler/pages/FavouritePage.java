c530cd1ed1051af0b52dbc83730ed1261fb283d2
Navakanthtunga <navakanthtunga@gmail.com>
2021-11-23 23:53:24 +0530

    Added changes for Remove favourites as per review comment for 1

--- src/test/java/com/webcrawler/pages/FavouritePage.java
+++ src/test/java/com/webcrawler/pages/FavouritePage.java
@@ -3,6 +3,10 @@ package com.webcrawler.pages;
 public class FavouritePage {
 
 	public void addFavourite() {
-		System.out.println("This is Favourite");
+		System.out.println("This is add Favourite");
+	}
+	
+	public void removeFavourite() {
+		System.out.println("This is remove Favourite");
 	}
 }
--- src/test/java/com/webcrawler/tests/FavouriteTest.java
+++ src/test/java/com/webcrawler/tests/FavouriteTest.java
@@ -3,6 +3,10 @@ package com.webcrawler.tests;
 public class FavouriteTest {
 
 	public void addFavouriteTests() {
-		System.out.println("This is Favourite Tests");
+		System.out.println("This is add Favourite Tests");
+	}
+	
+	public void removeFavouriteTests() {
+		System.out.println("This is remove Favourite Tests");
 	}
 }
