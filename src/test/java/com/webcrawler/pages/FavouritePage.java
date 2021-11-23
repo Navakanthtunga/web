5c1752ad5f47eea0ea7d81ddd9463f97fdb8ec43
Navakanthtunga <navakanthtunga@gmail.com>
2021-11-23 23:32:36 +0530

    Added Favourite Page, Test

---
+++ src/test/java/com/webcrawler/pages/FavouritePage.java
@@ -0,0 +1,8 @@
+package com.webcrawler.pages;
+
+public class FavouritePage {
+
+	public void addFavourite() {
+		System.out.println("This is Favourite");
+	}
+}
---
+++ src/test/java/com/webcrawler/tests/FavouriteTest.java
@@ -0,0 +1,8 @@
+package com.webcrawler.tests;
+
+public class FavouriteTest {
+
+	public void addFavouriteTests() {
+		System.out.println("This is Favourite Tests");
+	}
+}
