8af79531a029e8295a42f92b2089a77fde90643d
navakanthtunga <navakanthtunga@gmail.com>
2021-11-23 15:46:31 +0530

    Added Cart page and Test

---
+++ src/test/java/com/webcrawler/pages/CartPage.java
@@ -0,0 +1,8 @@
+package com.webcrawler.pages;
+
+public class CartPage {
+
+	public void AddCart() {
+		System.out.println("This is a Cart Page");
+	}
+}
---
+++ src/test/java/com/webcrawler/tests/CartTest.java
@@ -0,0 +1,7 @@
+package com.webcrawler.tests;
+
+public class CartTest {
+	public void CartTests() {
+		System.out.println("This is a Cart Test Function");
+	}
+}
