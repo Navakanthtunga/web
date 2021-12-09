d28d82673e0ea036b5ee6e4f5916a463cf2fd83d
navakanthtunga <navakanthtunga@gmail.com>
2021-12-09 15:44:09 +0530

    stash conflict resolved

--- src/test/java/com/webcrawler/tests/StashChanges.java
+++ src/test/java/com/webcrawler/tests/StashChanges.java
@@ -5,9 +5,15 @@ public class StashChanges {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+<<<<<<< Updated upstream
 		String str2 ="Car 2";
 		
 		System.out.println(str2);
+=======
+		String str1 ="Car 1";
+		
+		System.out.println(str1);
+>>>>>>> Stashed changes
 	}
 
 }
